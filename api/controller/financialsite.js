c59186bbe52b8b026cb3272b1f899a0c1541c2ca
jatin2306 <jatingupta2306@gmail.com>
2024-06-06 10:47:25 +0530

    zDsDDSDFS

--- api/controller/financialsite.js
+++ api/controller/financialsite.js
@@ -1,118 +1,130 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
 // get all user authentication data
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   try {
     res.json({
-  data:{
-  isloggedin:true
-}
+      data: {
+        isloggedin: true,
+      },
     });
   } catch (err) {
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
-router.post('/postlogin', async (req, res) => {
+router.post("/postlogin", async (req, res) => {
   try {
     res.json({
-response:[{
-  email:"user@gmail.com",
-password:"123456"
-}],
-        errorCode
-        : 
-        "201",
-        errorMessage
-        :
-        "Login Sucessfully",
-        httpStatusCode
-        : 
-        "201"
-
+      response: [
+        {
+          email: "user@gmail.com",
+          password: "123456",
+        },
+      ],
+      errorCode: "201",
+      errorMessage: "Login Sucessfully",
+      httpStatusCode: "201",
     });
   } catch (err) {
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).json({ error: "Internal Server Error" });
   }
-});router.post('/postvariable', async (req, res) => {
+});
+router.post("/postvariable", async (req, res) => {
   try {
     res.json({
-
-        errorCode
-        :
-        "201",
-        errorMessage
-        :
-        "Created Sucessfully",
-        httpStatusCode
-        :
-        "201"
-
+      errorCode: "201",
+      errorMessage: "Created Sucessfully",
+      httpStatusCode: "201",
     });
   } catch (err) {
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).json({ error: "Internal Server Error" });
   }
-});router.get('/getvariable', async (req, res) => {
+});
+
+router.get("/getvariable", async (req, res) => {
   try {
     res.json({
-response:[{
-  variableName:"name",
-  variable:"@name",
-  date:"23/3/2024",
-  },{
-    variableName:"name",
-    variable:"@name",
-    date:"23/3/2024",
-    },{
-      variableName:"name",
-      variable:"@name",
-      date:"23/3/2024",
-      },{
-        variableName:"name",
-        variable:"@name",
-        date:"23/3/2024",
-        },{
-          variableName:"name",
-          variable:"@name",
-          date:"23/3/2024",
-          },{
-            variableName:"name",
-            variable:"@name",
-            date:"23/3/2024",
-            },{
-              variableName:"name",
-              variable:"@name",
-              date:"23/3/2024",
-              },{
-                variableName:"name",
-                variable:"@name",
-                date:"23/3/2024",
-                },{
-                  variableName:"name",
-                  variable:"@name",
-                  date:"23/3/2024",
-                  },{
-                    variableName:"name",
-                    variable:"@name",
-                    date:"23/3/2024",
-                    },{
-                      variableName:"name",
-                      variable:"@name",
-                      date:"23/3/2024",
-                      },{
-                        variableName:"name",
-                        variable:"@name",
-                        date:"23/3/2024",
-                        }],
-    httpStatusCode: "200",
+      response: [
+        {
+          variableName: "name",
+          variable: "@name",
+          date: "23/3/2024",
+        },
+        {
+          variableName: "name",
+          variable: "@name",
+          date: "23/3/2024",
+        },
+        {
+          variableName: "name",
+          variable: "@name",
+          date: "23/3/2024",
+        },
+        {
+          variableName: "name",
+          variable: "@name",
+          date: "23/3/2024",
+        },
+        {
+          variableName: "name",
+          variable: "@name",
+          date: "23/3/2024",
+        },
+        {
+          variableName: "name",
+          variable: "@name",
+          date: "23/3/2024",
+        },
+        {
+          variableName: "name",
+          variable: "@name",
+          date: "23/3/2024",
+        },
+        {
+          variableName: "name",
+          variable: "@name",
+          date: "23/3/2024",
+        },
+        {
+          variableName: "name",
+          variable: "@name",
+          date: "23/3/2024",
+        },
+        {
+          variableName: "name",
+          variable: "@name",
+          date: "23/3/2024",
+        },
+        {
+          variableName: "name",
+          variable: "@name",
+          date: "23/3/2024",
+        },
+        {
+          variableName: "name",
+          variable: "@name",
+          date: "23/3/2024",
+        },
+      ],
+      httpStatusCode: "200",
       errorCode: "200",
-      errorMessage: ""
-
+      errorMessage: "",
     });
   } catch (err) {
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});router.post("/postclientmaster", async (req, res) => {
+  try {
+    res.json({
+      errorCode: "201",
+      errorMessage: "Created Sucessfully",
+      httpStatusCode: "201",
+    });
+  } catch (err) {
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
